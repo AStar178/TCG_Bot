3fa9ea50befb04e36d212c51cb564f4b1be5d9c6
AStar178 <AStar178Zero@gmail.com>
2022-07-01 14:13:38 +0430

    Speed+

--- TCG/Assets/Scripts/Battle/Abilities/Ability Type/MeleeAttack.cs
+++ TCG/Assets/Scripts/Battle/Abilities/Ability Type/MeleeAttack.cs
@@ -10,7 +10,7 @@ public abstract class MeleeAttack : Ability
 
     async void PlayerAttack(BattleS battle)
     {
-        Tween tween = battle.PlayerKnight.gameObject.transform.DOMoveX(battle.transform.position.x + 3, Util.Setting.Speed * .5f);
+        Tween tween = battle.PlayerKnight.gameObject.transform.DOMoveX(battle.transform.position.x + 3, Util.Setting.Speed * .25f);
         await tween.AsyncWaitForCompletion();
         tween = battle.PlayerKnight.gameObject.transform.DOMoveX(battle.transform.position.x + 4, Util.Setting.Speed * 0.125f);
         battle.PlayerKnight.gameObject.transform.DORotate(new Vector3(0,0,-16), Util.Setting.Speed * .5f);
@@ -21,8 +21,8 @@ public abstract class MeleeAttack : Ability
         battle.EnemyKnight.transform.DOShakePosition(.1f);
         await Task.Delay(10);
         battle.changeColor(Color.white, battle.EnemyKnight);
-        battle.PlayerKnight.gameObject.transform.DORotate(new Vector3(0, 0, 0), Util.Setting.Speed * .5f);
-        tween = battle.PlayerKnight.gameObject.transform.DOJump(new Vector3(battle.transform.position.x - 1, -3.5f, 0), 1,1,1);
+        battle.PlayerKnight.gameObject.transform.DORotate(new Vector3(0, 0, 0), Util.Setting.Speed * .25f);
+        tween = battle.PlayerKnight.gameObject.transform.DOJump(new Vector3(battle.transform.position.x - 1, -3.5f, 0), 1,1,Util.Setting.Speed * .25f);
         await tween.AsyncWaitForCompletion();
         tween = battle.PlayerKnight.gameObject.transform.DOMoveX(-5.5f, Util.Setting.Speed * .5f);
         await tween.AsyncWaitForCompletion();
