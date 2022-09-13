8817244b361f847ab63dc08a47d516cb1d82f48c
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-09-13 10:30:41 -0700

    owow

---
+++ Mir Nightimir/Assets/_script/EnemyState.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class EnemyState : MonoBehaviour {
+    
+    public State State;
+    public EnemyHp EnemyHp;
+    public TESTei tei;
+
+    private void Start() {
+        EnemyHp.MaxHp = State.MaxHpAmount;
+        EnemyHp.Currenthp = State.MaxHpAmount;
+        tei.Speed = State.MoveSpeed;
+    }
+
+}
