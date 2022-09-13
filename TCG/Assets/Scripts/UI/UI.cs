442c11067efc8eef47f136aeb316a40b22fcbe43
AStar178 <AStar178Zero@gmail.com>
2022-09-13 17:34:39 +0430

    Deleted

--- TCG/Assets/Scripts/UI/UI.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class UI : MonoBehaviour
-{
-    public List<GameObject> Attack_Buttom = new List<GameObject>();
-}
