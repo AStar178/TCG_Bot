bfeffe85a73db44b685b5d20e700616b5e3ec103
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-09-13 10:19:55 -0700

    Update PlayerHp.cs

--- Mir Nightimir/Assets/_script/PlayerHp.cs
+++ Mir Nightimir/Assets/_script/PlayerHp.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerHp : MonoBehaviour, IHpValue
 {
+    [SerializeField] Player player;
     public float MaxHp;
     public float Currenthp;
-
+    public float DelayDamageTakeTime;
+    float delayTime;
     private void Start() {
         Currenthp = MaxHp;
     }
+    private void Update() {
+
+        if (delayTime <= 0)
+            return;
+        
+
+        delayTime -= Time.deltaTime;
+    }
     public void HpValueChange(Damage damage)
     {
-        
+        if (delayTime >= 0)
+            return;
+
+        delayTime = DelayDamageTakeTime;
+        float AdDamageAmount = 100 - player.PlayerState.Ap_Defence;
+        Currenthp -= damage.AdDamage * ( AdDamageAmount / 100 );
+
     }
 }
