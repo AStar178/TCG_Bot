07a4c1d939e80e9e203fde180d74f75a49bfbbe0
AStar178 <AStar178Zero@gmail.com>
2022-09-13 23:41:44 +0430

    Bizzzzzzzzzzzzzz

--- Mir Nightimir/Assets/_script/Enemy AI/RangeED.cs
+++ Mir Nightimir/Assets/_script/Enemy AI/RangeED.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class RangeED : TESTei
@@ -24,17 +25,20 @@ public class RangeED : TESTei
     public override void update()
     {
         if (Vector2.Distance(gameObject.transform.position, target.transform.position) <= Range)
-        { transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Speed * Time.deltaTime); }
-        if (Vector2.Distance(gameObject.transform.position, target.transform.position) <= 1 + Range * 0.1f)
-        { transform.position = Vector2.MoveTowards(transform.position, target.transform.position, -Speed * Time.deltaTime); ATTACK = true; }
+        { transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Speed * Time.deltaTime); ATTACK = true; }
         else ATTACK = false;
+        if (Vector2.Distance(gameObject.transform.position, target.transform.position) <= 1 + Range * 0.1f)
+        { transform.position = Vector2.MoveTowards(transform.position, target.transform.position, -Speed * Time.deltaTime); }
 
 
         if (ATTACK == true)
         {
             if (TBS <= 0)
             {
-                Instantiate(project, transform.position, Quaternion.identity);
+                GameObject B = Instantiate(project, transform.position, Quaternion.identity);
+                Vector3 targetPos = FindObjectOfType<PlayerMoveMent>().transform.position;
+                B.transform.DOMove(targetPos, .5f);
+                Destroy(B, .5f);
                 if (FixSecond >= FixSecondN)
                 {
                     TBS = STBS;
