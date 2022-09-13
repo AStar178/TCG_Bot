2d1ab8e627ac8ea34ebb87d71d939775389247a4
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-09-13 07:33:47 -0700

    wow

--- Mir Nightimir/Assets/_script/PlayerMoveMent.cs
+++ Mir Nightimir/Assets/_script/PlayerMoveMent.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,29 @@ using UnityEngine;
 public class PlayerMoveMent : MonoBehaviour
 {
     [SerializeField] Rigidbody2D body3d;
+    [SerializeField] SpriteRenderer SpriteRenderer;
     [SerializeField] float moveSpeed;
     float movementX;
     float movementY;
-
 
     void Update()
     {
         movementX = Input.GetAxisRaw("Horizontal");
         movementY = Input.GetAxisRaw("Vertical");
-
+    
         body3d.velocity = new Vector2(movementX , movementY).normalized * moveSpeed;
+        SpriteUpdaye();
+
+    }
+
+    private void SpriteUpdaye()
+    {
+        if (new Vector2(movementX , movementY).normalized == Vector2.zero) { return; }
+        if (Vector2.Dot(new Vector2(movementX , movementY).normalized , Vector2.right) < -0.1f)
+        {
+            SpriteRenderer.flipX = true;
+            return;
+        } 
+        SpriteRenderer.flipX = false;
     }
 }
