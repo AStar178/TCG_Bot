661e717554f2b0288d7c00e1973b5316b1df1fb4
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-09-15 05:02:00 -0700

    da

--- Mir Nightimir/Assets/_script/PlayerMoveMent.cs
+++ Mir Nightimir/Assets/_script/PlayerMoveMent.cs
@@ -10,6 +10,8 @@ public class PlayerMoveMent : MonoBehaviour
     [SerializeField] Rigidbody2D body3d;
     [SerializeField] public SpriteRenderer SpriteRenderer;
     [HideInInspector] public float moveSpeed;
+    [SerializeField] private Transform BulletSpawnPos;
+    [SerializeField] private Vector2 pos;
     float movementX;
     float movementY;
 
@@ -33,8 +35,10 @@ public class PlayerMoveMent : MonoBehaviour
         if (Vector2.Dot(new Vector2(movementX , movementY).normalized , Vector2.right) < -0.1f)
         {
             SpriteRenderer.flipX = true;
+            BulletSpawnPos.localPosition = ( new Vector2( pos.x * -1 , pos.y ) );            
             return;
         } 
         SpriteRenderer.flipX = false;
+        BulletSpawnPos.localPosition = pos;
     }
 }
