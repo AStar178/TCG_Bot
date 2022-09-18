909260ec694d679bff9d9f2043a607fc8036fcc8
AStar178 <AStar178Zero@gmail.com>
2022-09-18 17:14:34 +0430

    Yurret with enemy status

--- Mir Nightimir/Assets/_script/EnemyState.cs
+++ Mir Nightimir/Assets/_script/EnemyState.cs
@@ -4,16 +4,31 @@ public class EnemyState : MonoBehaviour {
     
     public State State;
     public EnemyHp EnemyHp;
+    [HideInInspector]
     public TESTei tei;
+    [HideInInspector]
+    public Turret yurret;
 
     private void Start() {
-        tei = gameObject.GetComponent<TESTei>();
+        EnemyHp = gameObject.GetComponent<EnemyHp>();
+        gameObject.TryGetComponent<Turret>(out Turret nodle);
+        gameObject.TryGetComponent<TESTei>(out TESTei Makaroni);
+
+        if (nodle != null)
+        {
+            yurret.Range = State.AggroRange;
+            yurret.stat = State;
+        }
+
+        if (Makaroni != null)
+        {
+            tei.Speed = State.MoveSpeed;
+            tei.Range = State.AggroRange;
+            tei.state = State;
+        }
 
         EnemyHp.MaxHp = State.MaxHpAmount;
         EnemyHp.Currenthp = State.MaxHpAmount;
-        tei.Speed = State.MoveSpeed;
-        tei.Range = State.AggroRange;
-        tei.state = State;
         EnemyHp.Amoro = State.Amoro;
         EnemyHp.MagicResest = State.MagicReset;
     }
