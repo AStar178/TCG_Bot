8632a7a567aef8075c4ecb2588aa7199d45555ce
AStar178 <AStar178Zero@gmail.com>
2022-09-18 20:37:32 +0430

    Asorry

--- Mir Nightimir/Assets/_script/Enemy AI/EnemyStatic.cs
+++ Mir Nightimir/Assets/_script/Enemy AI/EnemyStatic.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
+using DG.Tweening;
 
 public class EnemyStatic : MonoBehaviour
 {
@@ -13,5 +15,26 @@ public class EnemyStatic : MonoBehaviour
     {
         project = Project;
         textPrefab = TextPrefab;
+    }
+    public static void CreatCoustomTextPopup(string v, Vector3 position, Color color)
+    {
+        var text = Instantiate(EnemyStatic.textPrefab, position, Quaternion.identity);
+        TMPro.TMP_Text tMP_Text = text.GetComponentInChildren<TMPro.TMP_Text>();
+        tMP_Text.text = v;
+        tMP_Text.color = color;
+        Tween tween = tMP_Text.transform.DOMoveY(position.y += 1.25f, 1f);
+        KillTween(1f, tween, tMP_Text.transform.gameObject.transform.gameObject);
+        Destroy(text, 2);
+    }
+    private static async void KillTween(float v, Tween tween, GameObject b)
+    {
+        float zz = v;
+        while (zz > 0)
+        {
+            zz -= Time.deltaTime;
+            await Task.Yield();
+        }
+        tween.Kill();
+        Destroy(b);
     }
 }
--- Mir Nightimir/Assets/_script/Enemy AI/Misc/Makalaka.cs
+++ Mir Nightimir/Assets/_script/Enemy AI/Misc/Makalaka.cs
@@ -14,38 +14,18 @@ public class Makalaka : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Speed * Time.deltaTime);
+        if (target == null)
+        {
+            EnemyStatic.CreatCoustomTextPopup("Dissapeared", gameObject.transform.position, Color.gray);
+            Destroy(gameObject);
+            return;
+        }
         if (Vector2.Distance(target.transform.position, transform.position) <= .5f)
         {
             BozzHP.Currenthp = BozzHP.Currenthp + Healing;
-            CreatCoustomTextPopup("+" + Healing, target.transform.position);
+            EnemyStatic.CreatCoustomTextPopup("+" + Healing, target.transform.position, Color.green);
             Destroy(gameObject);
         }
-        if (target == null)
-        {
-            Destroy(gameObject);
-            CreatCoustomTextPopup("Dissapeared", gameObject.transform.position);
-        }
-    }
-    public void CreatCoustomTextPopup(string v, Vector3 position)
-    {
-        var text = Instantiate(EnemyStatic.textPrefab, position, Quaternion.identity);
-        TMPro.TMP_Text tMP_Text = text.GetComponentInChildren<TMPro.TMP_Text>();
-        tMP_Text.text = v;
-        tMP_Text.color = Color.yellow;
-        Tween tween = tMP_Text.transform.DOMoveY(position.y += 1.25f, 1f);
-        KillTween(1f, tween, tMP_Text.transform.gameObject.transform.gameObject);
-        Destroy(text, 2);
-    }
-    private async void KillTween(float v, Tween tween, GameObject b)
-    {
-        float zz = v;
-        while (zz > 0)
-        {
-            zz -= Time.deltaTime;
-            await Task.Yield();
-        }
-        tween.Kill();
-        Destroy(b);
+        transform.position = Vector2.MoveTowards(transform.position, target.transform.position, Speed * Time.deltaTime);
     }
 }
