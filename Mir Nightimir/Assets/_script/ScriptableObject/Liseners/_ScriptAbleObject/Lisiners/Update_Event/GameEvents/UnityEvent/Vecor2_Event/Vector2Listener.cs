9483cc1891d9eaf7d08e26e3574b3ce2a4c1513a
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-12-22 11:36:30 -0800

    sus

--- Mir Nightimir/Assets/_script/ScriptableObject/Liseners/_ScriptAbleObject/Lisiners/Update_Event/GameEvents/UnityEvent/Vecor2_Event/Vector2Listener.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Vector2Listener : BaseGameEventListener<Vector2 , Vector2Event , UnityVector2Event> {  }
-
