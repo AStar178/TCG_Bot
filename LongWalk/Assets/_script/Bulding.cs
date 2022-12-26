9bd0fc824f5a6f4d1197aed2be7ab2fb8aa32e5a
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-12-26 09:34:06 -0800

    sa

---
+++ LongWalk/Assets/_script/Bulding.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulding : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> _BulidingBlockPrefabs;
+    [SerializeField] private float MaxDistance;
+    [SerializeField] private LayerMask GroundLayer;
+
+    void Update()
+    {
+        Vector3 Pos = new Vector3();
+
+        Physics.Raycast(transform.position , transform.forward , out RaycastHit hit , MaxDistance , GroundLayer );
+
+        if (hit.collider == null)
+            return;
+        Pos = hit.point;            
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            Instantiate(Build(1 - 1)  , Pos , transform.rotation);
+        
+        
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            Instantiate(Build(2 - 1)  , Pos , transform.rotation);
+
+    }
+
+    private GameObject Build(int index)
+    {
+        for (int i = 0; i < _BulidingBlockPrefabs.Count; i++)
+        {
+            if (i == index)
+                return _BulidingBlockPrefabs[i];
+        }
+
+        return null;
+    } 
+
+    private void OnDrawGizmos() {
+        
+        Gizmos.DrawLine(transform.position , transform.position + transform.forward * MaxDistance);
+
+    }
+}
