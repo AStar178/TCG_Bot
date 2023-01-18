ee4667a0a9fd7ea38769a3f4ea5078c6bf264907
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2023-01-18 08:12:09 -0800

    sda

--- Riskofmiz3q/Assets/_script/Player.cs
+++ Riskofmiz3q/Assets/_script/Player.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
     {
-        
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
