e99c4335103390ee9a780f3bec74a0f8e059af6a
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2023-01-20 09:49:37 -0800

    da

--- Riskofmiz3q/Assets/_script/LofyEffect.cs
+++ Riskofmiz3q/Assets/_script/LofyEffect.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class LofyEffect : MonoBehaviour
 {
-    
+    [SerializeField] Playermovement movement;
+    [SerializeField] Material material;
+    [SerializeField] float Sad;
+    [SerializeField] Animator animationX;
+    float s;
     void Update() 
     {
-
+        s = 0.3f;
+        bool sa = false;
+        if (movement.MovementDir.magnitude > 0)
+        {
+            s = Sad + Random.Range(-0.2f , 0.2f);
+            sa = true;
+        }
+        animationX.SetBool("run" , sa);
+        material.SetFloat("_Float_4" , Mathf.Lerp(material.GetFloat("_Float_4") , s , 0.03f));
     }
 
 }
--- Riskofmiz3q/Assets/_script/Playershoot.cs
+++ Riskofmiz3q/Assets/_script/Playershoot.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Playershoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    [SerializeField] Camera cameras;
+    [SerializeField] Transform ora;
+    [SerializeField] Playermovement movement; 
+    private void Start() => cameras = Camera.main;
     void Update()
     {
-        
+        if (movement.MovementDir.magnitude == 0)
+            return;
+
+        Vector3 normalized = (cameras.transform.position - ora.transform.position).normalized * -1;
+        ora.localRotation = Quaternion.Lerp( ora.localRotation , Quaternion.LookRotation(new Vector3(normalized.x , 0 ,normalized.z)) , 0.1f ); 
     }
 }
