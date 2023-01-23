2c64684f18e12999d28d233465fe391eed117c12
AStar178 <AStar178Zero@gmail.com>
2023-01-23 22:00:17 +0330

    Fixed

--- Riskofmiz3q/Assets/_script/SkillManager.cs
+++ Riskofmiz3q/Assets/_script/SkillManager.cs
@@ -13,19 +13,19 @@ public abstract class SkillManager : MonoBehaviour
     // Update is called once per frame
     public virtual void Update()
     {
-        if (Input.GetKeyDown(RougeLiter.rougeLiter.InputManager.Skill_1))
+        if (Input.GetKeyDown(RougeLiter.InputManager.Skill_1))
         {
             OnSkill_1();
         }
-        if (Input.GetKeyDown(RougeLiter.rougeLiter.InputManager.Skill_2))
+        if (Input.GetKeyDown(RougeLiter.InputManager.Skill_2))
         {
             OnSkill_2();
         }
-        if (Input.GetKeyDown(RougeLiter.rougeLiter.InputManager.Skill_3))
+        if (Input.GetKeyDown(RougeLiter.InputManager.Skill_3))
         {
             OnSkill_3();
         }
-        if (Input.GetKeyDown(RougeLiter.rougeLiter.InputManager.Skill_4))
+        if (Input.GetKeyDown(RougeLiter.InputManager.Skill_4))
         {
             OnSkill_4();
         }
