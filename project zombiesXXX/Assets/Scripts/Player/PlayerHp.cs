0472f57e5fcb349656c03db3b5052479942b30c5
AStar178 <AStar178Zero@gmail.com>
2023-07-14 13:10:25 +0430

    Player ho max value fixed
    but not the damage take :sob:

--- project zombiesXXX/Assets/Scripts/Player/PlayerHp.cs
+++ project zombiesXXX/Assets/Scripts/Player/PlayerHp.cs
@@ -7,7 +7,7 @@ public class PlayerHp : PlayerComponetSystem , IDamageAble
     void IDamageAble.TakeDamage(DamageData Data)
     {
         float Hp = GetHpCurrent - Data.DamageAmount;
-        Hp = Mathf.Clamp( Hp , 0 , 100 );
+        Hp = Mathf.Clamp( Hp , 0 , Player.PlayerState.ResultValue.HpMax );
         SetHpCurrent(Hp);
         Debug.Log("toke '" + Data.DamageAmount + "' damage");
         if (GetHpCurrent <= 0)
