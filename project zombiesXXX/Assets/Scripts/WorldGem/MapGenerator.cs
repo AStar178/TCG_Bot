3ac91305b6c09c10dfd7c1d07d29e7ef0484dcd6
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2023-07-27 05:42:33 +0330

    wordGENERASHIN!!!!!!

--- project zombiesXXX/Assets/Scripts/WorldGem/MapGenerator.cs
+++ project zombiesXXX/Assets/Scripts/WorldGem/MapGenerator.cs
@@ -3,7 +3,7 @@ using System.Linq;
 
 public class MapGenerator : MonoBehaviour {
     
-    public enum DrawMode {NoiseMap, ColourMap};
+    public enum DrawMode {NoiseMap, ColourMap , Mesh};
     public DrawMode drawMode;
     public int MapWith;
     public int mapHieght;
@@ -16,50 +16,30 @@ public class MapGenerator : MonoBehaviour {
     public Vector2 offset;
     public int seed;
     public TrainnType[] regions;
-    [Range(0 , 1)]
-    public float closenes;
+    public float height;
+    float[,] falloffmap;
+    public AnimationCurve meshheightcurve;
+    private void Awake() {
+        
+        falloffmap = FalloffGenerator.GenerateFalloffMap((int)MapWith);
+        gameObject.AddComponent<MeshCollider>();
+        GenerateMap();
+    }
     public void GenerateMap()
     {
         var noisemap = Noise.GenerateNoiseMap(MapWith , mapHieght, seed , Sacale , octives , pernalenoise , lacunarity , offset);
-
+        falloffmap = FalloffGenerator.GenerateFalloffMap((int)MapWith);
         MapDisplay mapDisplay = FindAnyObjectByType<MapDisplay>();
 
         Color[] colourMap = new Color[MapWith * mapHieght];
 		for (int y = 0; y < mapHieght; y++) {
 			for (int x = 0; x < MapWith; x++) {
+                
+                noisemap[x,y] = Mathf.Clamp01( noisemap[x,y] - falloffmap[x , y] );
 				float currentHeight = noisemap [x, y];
 				for (int i = 0; i < regions.Length; i++) {
 					if (currentHeight <= regions [i].height) {
                         Color color = regions [i].coloir;
-
-
-                        if ((regions.Count() > i+1))
-                        {
-                            var colorwow = regions[i+1].coloir;
-
-                                var starte = (regions[i].height + ((regions[i+1].height - regions[i].height)* closenes));
-                                var owo = regions[i+1].height - starte;
-                                var currentpos = regions[i+1].height - currentHeight;
-                                var wopw = owo - currentpos;
-                                var yes = wopw/owo;
-                                
-                                    
-
-                                color = Color.Lerp(color , colorwow , yes);
-                                
-                            
-
-                                
-                            
-                            
-                        }
-
-
-
-
-
-
-
 						colourMap [y * MapWith + x] = color;
 						break;
 					}
@@ -72,6 +52,8 @@ public class MapGenerator : MonoBehaviour {
 			display.DrawTexture (TextureGenerator.TextureFromHeightMap(noisemap));
 		} else if (drawMode == DrawMode.ColourMap) {
 			display.DrawTexture (TextureGenerator.TextureFromColourMap(colourMap , mapHieght , mapHieght));
+		} else if (drawMode == DrawMode.Mesh) {
+			display.DrawMesh (MeshGenerator.GenerateTerrainMesh(noisemap , height , meshheightcurve) , TextureGenerator.TextureFromColourMap(colourMap , mapHieght , mapHieght));
 		}
     }
 
